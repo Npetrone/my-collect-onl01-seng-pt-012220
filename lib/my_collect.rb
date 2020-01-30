98b201e3f256bb1e273483f9c99f4d8d912e8e18
Nicholas Petrone <nickpetrone92@gmail.com>
2020-01-30 18:32:37 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
 
 
+
+
+
+
+
+def my_collect(array)
+  counter = 0
+  new_array = []
+  while counter < array.length
+    array << yield(array[counter])
+    counter += 1
+  end
+  new_array
+end
+
+
